b8953710f62cf960117150bcb371654735962262
vgsamsonov <114337125+vgsamsonov@users.noreply.github.com>
2025-02-23 20:25:00 +0300

    Update sust1.sh
    Добавил output и возможность задать свои правила

--- sust1.sh
+++ sust1.sh
@@ -3,13 +3,18 @@
 # Параметры по умолчанию
 PROJECT_PATH=""
 ANALYSIS_MODE="fast"
+BEARER_RULES=""
+SEMGREP_RULES=""
+TIMESTAMP=$(date +%Y-%m-%d_%H:%M:%S)
 
 show_help() {
     echo "Использование: $0 [опции]"
     echo ""
     echo "Опции:"
-    echo "  -p, --path <путь>      Путь к проекту для анализа"
-    echo "  -m, --mode <режим>     Режим анализа (fast/full)" # Пока есть только fast
+    echo "  -p, --path <путь>        Путь к проекту для анализа"
+    echo "  -m, --mode <режим>       Режим анализа (fast/full)"
+    echo "  -b, --bearer-rules <путь> Путь к правилам bearer"
+    echo "  -s, --semgrep-rules <путь> Путь к правилам semgrep"
     exit 1
 }
 
@@ -22,6 +27,14 @@ while [[ $# -gt 0 ]]; do
             ;;
         -m|--mode)
             ANALYSIS_MODE="$2"
+            shift 2
+            ;;
+        -b|--bearer-rules)
+            BEARER_RULES="$2"
+            shift 2
+            ;;
+        -s|--semgrep-rules)
+            SEMGREP_RULES="$2"
             shift 2
             ;;
         -h|--help)
@@ -40,12 +53,25 @@ if [ -z "$PROJECT_PATH" ]; then
     show_help
 fi
 
-
 # Быстрый анализ (semgrep/bearer)
 run_fast_analysis() {
     echo "Запуск быстрой проверки..."
-    semgrep scan --config=/tmp/semgrep-rules "$PROJECT_PATH"
-    bearer scan "$PROJECT_PATH"
+    
+    # Запуск semgrep с правилами, если они указаны
+    if [ -n "$SEMGREP_RULES" ]; then
+        semgrep scan --config="$SEMGREP_RULES" "$PROJECT_PATH" -o semgrep_report_${TIMESTAMP}.log
+    else
+        semgrep scan --config=auto "$PROJECT_PATH" -o semgrep_report_${TIMESTAMP}.log
+    fi
+    
+    # Запуск bearer с правилами, если они указаны
+    if [ -n "$BEARER_RULES" ]; then
+        bearer scan "$PROJECT_PATH" --external-rule-dir "$BEARER_RULES" --output bearer_sast_report_${TIMESTAMP}.log
+        bearer scan "$PROJECT_PATH" --external-rule-dir "$BEARER_RULES" --scanner secrets --output bearer_secrets_report_${TIMESTAMP}.log
+    else
+        bearer scan "$PROJECT_PATH" --output bearer_sast_report_${TIMESTAMP}.log
+        bearer scan "$PROJECT_PATH" --scanner secrets --output bearer_secrets_report_${TIMESTAMP}.log
+    fi
 }
 
 # Основной процесс
